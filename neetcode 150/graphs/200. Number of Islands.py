835f4dfd37afbe95c57b653ce6909de5e81e8382
Char Z <2003charz@gmail.com>
2024-07-17 15:15:41 -0700

    graphs are easy as usual

--- neetcode 150/graphs/200. Number of Islands.py
+++ neetcode 150/graphs/200. Number of Islands.py
@@ -1,84 +1,36 @@
-
-# idea: iterate through each index
-# with 2 for loops, O(M*N)
-# when reaching a '1', perform BFS on that
-# each time adding the visited values
+import collections
 
 
-def numIslands(grid: list[list[str]]) -> int:    
-    if not grid:
-        return 0
-    
-    rows, cols = len(grid), len(grid[0])
-    visited = set()
-    islands = 0
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        seen = set()
+        count = 0
 
-    def BFS(i,j):
-        queue = [(i,j)]
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if (i,j) in seen or grid[i][j]=='0': continue
 
-        while queue:
-            curr = queue.pop()
-            i,j = curr[0], curr[1]
-            visited.add((i,j))
-            
-            # add all neighbors thats not in visited and not out of bounds
-            if grid[i][j] == '1':
-                if (not i==0) and ((i-1,j) not in visited):
-                    queue.append((i-1,j))
-                if (not i==rows-1) and ((i+1,j) not in visited):
-                    queue.append((i+1,j))
-                if (not j==0) and ((i,j-1) not in visited):
-                    queue.append((i,j-1))
-                if (not j==cols-1) and ((i,j+1) not in visited):
-                    queue.append((i,j+1))
+                queue = collections.deque()
+                queue.append((i,j))
+
+                while queue:
+                    x, y = queue.popleft()
+                    if x<0 or x>len(grid)-1 or y<0 or y>len(grid[0])-1 or grid[x][y] == '0' or (x,y) in seen: continue
+                    seen.add((x,y))
+                    queue.extend([(x+1, y), (x-1, y), (x, y+1), (x, y-1)])
+                count+=1
+
+        return count
 
 
-    # neetcode BFS
-    # import collections
-    # def bfs(r,c):
-        # q = collections.deque()
-        # visited.add((r,c))
-        # q.append((r,c))
-    
-        # while q:
-        #     row,col = q.popleft()
-        #     directions= [[1,0],[-1,0],[0,1],[0,-1]]
-        
-        #     for dr,dc in directions:
-        #         r,c = row + dr, col + dc
-        #         if (r) in range(rows) and (c) in range(cols) and grid[r][c] == '1' and (r ,c) not in visited:
-                
-        #             q.append((r , c ))
-        #             visited.add((r, c ))
+sol = Solution()
+# grid = [
+#   ["1","1","1","1","0"],
+#   ["1","1","0","1","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","0","0","0"]
+# ]
+# print(sol.numIslands(grid))
 
-    for i in range(rows):
-        for j in range(cols):
-            # if (i,j) in visited:
-            #     continue
-
-            if grid[i][j] == '1' and (i, j) not in visited:
-                islands+=1
-                BFS(i,j)
-
-    return islands
-
-
-grid = [
-  ["1","1","1","1","0"],
-  ["1","1","0","1","0"],
-  ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
-]
-
-grid = [
-  ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
-]
-
-# grid = [["1","1","1"],["0","1","0"],["1","1","1"]]
-
-
-
-print(numIslands(grid))
+grid = [["1","0"]]
+print(sol.numIslands(grid))
--- neetcode 150/trees/230. Kth Smallest Element in a BST.py
+++ neetcode 150/trees/230. Kth Smallest Element in a BST.py
@@ -1,53 +1,14 @@
 from binarytree import *
 
-# assume n>k
-# worst case O(n) time, O(n) space for recursive stack
+class Solution:
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
+        def helper(node):
+            if not node: return []
+            return helper(node.left) + [node.val] + helper(node.right)
+        return helper(root)[k-1]
 
 
-# doesn't work but is close enough
-# leetcode buggy????
-def kthSmallest(root: TreeNode, k: int) -> int:
-    def dfs(node, c):
-        if not node:
-            return 0, None
-            
-        left_c, left_ret = dfs(node.left, c)
-        if left_ret:
-            return left_c, left_ret
-        
-        c+=left_c
-        c+=1
-        
-        if c==k:
-            return c, node.val
-
-        right_c, right_ret = dfs(node.right, c)
-        if right_ret:
-            return right_c, right_ret
-        
-        return c+right_c, None
-    
-    return dfs(root, 0)[1]
-
-
-# iterative solution
-def kthSmallest(root: TreeNode, k: int) -> int:
-    stack = []
-    curr = root
-
-    while stack or curr:
-        # follow left side of tree, add all nodes
-        while curr:
-            stack.append(curr)
-            curr = curr.left
-
-        curr = stack.pop()
-        k-=1
-        if k == 0:
-            return curr.val
-        curr = curr.right
-
-
-test = [3,1,4,None,2]
-root = to_binary_tree(test)
-print(kthSmallest(root,3))
+sol = Solution()
+root = toBinaryTree([3,1,4,None,2])
+k = 1
+print(sol.kthSmallest(root, k))
