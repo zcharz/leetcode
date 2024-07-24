3e83558b78176a376c46c1b39d7e46fbb4028371
Char Z <2003charz@gmail.com>
2024-07-23 17:17:04 -0700

    not time for more today :(

---
+++ neetcode 150/graphs/417. Pacific Atlantic Water Flow.py
@@ -0,0 +1,7 @@
+class Solution:
+    def pacificAtlantic(self, heights: list[list[int]]) -> list[list[int]]:
+        pass
+
+sol = Solution()
+heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+print(sol.pacificAtlantic(heights))
--- neetcode 150/heap/703. Kth Largest Element in a Stream.py
+++ neetcode 150/heap/703. Kth Largest Element in a Stream.py
@@ -1,34 +1,31 @@
 import heapq
 
 class KthLargest:
+
     def __init__(self, k: int, nums: list[int]):
-        self.k = k
-        heapq.heapify(nums)
-
-        while k<len(nums):
-            heapq.heappop(nums)
         self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
 
-
+        while len(self.nums)>self.k:
+            heapq.heappop(self.nums)
+        
     def add(self, val: int) -> int:
         heapq.heappush(self.nums, val)
-
         if len(self.nums)>self.k:
             heapq.heappop(self.nums)
-        # initialization edge case with less elements in nums than k
-
-        # min heap has min value at index 0
-        # this is the kth largest
         return self.nums[0]
-        
-    
 
-k = 3
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
+
+
 nums = [4, 5, 8, 2]
-
-test = KthLargest(k, nums)
-
-add_list = [3,5,10,9,4]
-
-for i in add_list:
-    print(test.add(i))
+sol = KthLargest(3, nums)
+print(sol.add(3))
+print(sol.add(5))
+print(sol.add(10))
+print(sol.add(9))
+print(sol.add(4))
