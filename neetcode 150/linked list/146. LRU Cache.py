7ea5924c86b290bf02ec5da00c1b01d556c7bd61
Char Z <2003charz@gmail.com>
2024-08-14 13:31:16 -0700

    round 2 with LRU cache, DONE with linkedlists

--- neetcode 150/linked list/146. LRU Cache.py
+++ neetcode 150/linked list/146. LRU Cache.py
@@ -1,20 +1,57 @@
-from linkedlist import *
+class ListNode:
+    def __init__(self, key=0, val=0, prev=None, next=None):
+        self.key = key
+        self.val = val
+        self.prev = prev
+        self.next = next
+
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.time = 0
-        self.order = [0 for i in range(capacity)]
+        self.front = ListNode()
+        self.back = ListNode()
         self.map = dict()
+        self.capacity = capacity
+
+        self.front.next = self.back
+        self.back.prev = self.front
 
     def get(self, key: int) -> int:
         if key in self.map:
-            return self.map[key]
+            self.updateFront(key)
+            return self.map[key].val
         return -1
 
     def put(self, key: int, value: int) -> None:
-        self.map[key] = value
+        if key in self.map:
+            self.updateFront(key)
+            self.map[key].val = value
+            return
+
+        if len(self.map.keys()) == self.capacity:
+            deletenode = self.back.prev
+
+            self.back.prev = self.back.prev.prev
+            self.back.prev.next = self.back
+            
+            del self.map[deletenode.key]
 
 
-sol = LRUCache()
+        newnode = ListNode(key,value)
+        self.map[key] = newnode
+        self.updateFront(key)
+        
 
+    def updateFront(self, key):
+        currnode = self.map[key]
+
+        if currnode.prev and currnode.next:
+            currnode.prev.next = currnode.next
+            currnode.next.prev = currnode.prev
+
+        self.front.next.prev = currnode
+        currnode.next = self.front.next
+
+        currnode.prev = self.front
+        self.front.next = currnode
