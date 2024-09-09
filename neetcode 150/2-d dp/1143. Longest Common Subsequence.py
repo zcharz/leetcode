d7bf688638583fe291435198f6d6a8b833ae99ee
Char Z <2003charz@gmail.com>
2024-09-08 22:15:40 -0700

    gonna need to watch the video on some of these... but at least i did unique paths

---
+++ neetcode 150/2-d dp/1143. Longest Common Subsequence.py
@@ -0,0 +1,8 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        pass
+
+sol = Solution()
+text1 = "abcde"
+text2 = "ace" 
+print(sol.longestCommonSubsequence(text1, text2))
--- neetcode 150/intervals/435. Non-overlapping Intervals.py
+++ neetcode 150/intervals/435. Non-overlapping Intervals.py
@@ -1,3 +1,12 @@
 class Solution:
-    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+    def eraseOverlapIntervals(self, intervals: list[list[int]]) -> int:
+        intervals.sort()
+        l, r = 0, 1
+
+        while r<len(intervals):
+            if 
         
+
+sol = Solution()
+intervals = [[1,2],[2,3],[3,4],[1,3]]
+print(sol.eraseOverlapIntervals(intervals))
