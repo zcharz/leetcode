77b76b96b7a892b9cc4ce28ea8ba07d3033f33c6
Char Z <2003charz@gmail.com>
2024-09-13 15:34:59 -0700

    dykestra

--- neetcode 150/advanced graphs/743. Network Delay Time.py
+++ neetcode 150/advanced graphs/743. Network Delay Time.py
@@ -1,11 +1,37 @@
-# create shortest paths table
-# populate table with dijkstras
-# if all nodes have a value
-#    return max of paths to other verticies 
-# if some nodes can't be reached
-#    return -1
+import heapq
 
 
 class Solution:
     def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
-        pass
+            adjacency_list = {i+1:[] for i in range(n)}
+            for u, v, w in times: adjacency_list[u].append((v,w))
+
+            minheap = [(0, k)]
+            dist = {vertex:float('inf') for vertex in adjacency_list}
+            dist[k] = 0
+
+            while minheap:
+                curr_dist, curr_vertex = heapq.heappop(minheap)
+                for next_vertex, weight in adjacency_list[curr_vertex]:
+                    if dist[next_vertex] > curr_dist + weight:
+                        dist[next_vertex] = curr_dist + weight
+                        heapq.heappush(minheap, (dist[next_vertex], next_vertex))
+
+            currmax = 0
+            for delay in dist.values():
+                if delay == float('inf'): return -1
+                currmax = max(currmax, delay)
+            return currmax
+    
+    
+sol = Solution()
+
+times = [[2,1,1],[2,3,1],[3,4,1]]
+n = 4
+k = 2
+print(sol.networkDelayTime(times, n, k))
+
+times = [[1,2,1]]
+n = 2
+k = 1
+print(sol.networkDelayTime(times, n, k))
--- neetcode 150/greedy/45. Jump Game II.py
+++ neetcode 150/greedy/45. Jump Game II.py
@@ -1,18 +1,17 @@
 class Solution:
     def jump(self, nums: list[int]) -> int:
-        count, pos = 0, 0
+        steps = 0
+        l = r = 0
 
-        while pos < len(nums)-1:
-            currdist = 0
-            ind = 0
-            for i in range(pos+1, nums[pos]+1):
-                if i+nums[i]> currdist: 
-                    currdist = i+nums[i]
-                    ind = i
-            count+=1
-            pos = ind
+        while r<len(nums)-1:
+            maxnext = 0
+            for i in range(l, r+1):
+                maxnext = max(maxnext, i + nums[i])
+            l = r+1
+            r = maxnext
+            steps+=1
 
-        return count
+        return steps
 
 sol = Solution()
 
@@ -23,4 +22,7 @@ nums = [2,3,0,1,4]
 print(sol.jump(nums))
 
 nums = [1,2]
+print(sol.jump(nums))
+
+nums = [4,1,1,3,1,1,1]
 print(sol.jump(nums))
